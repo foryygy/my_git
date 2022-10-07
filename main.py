265d23e5d96cb448a8cce950c0f5a07862b3f36d
foryygy <835566878@qq.com>
2022-10-07 14:16:22 +0800

    v1

---
+++ main.py
@@ -0,0 +1,26 @@
+class MyType(type):
+    def __init__(self, *args, **kwargs):
+        print('init')
+        super(MyType, self).__init__(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        print('call本质：调用类的__new__，再调用类的__init__')
+        return super(MyType, self).__call__(*args, **kwargs)
+
+
+# class Foo(metaclass=MyType):
+#     pass
+#
+#
+# class Bar(Foo):
+#     pass
+
+Foo = MyType("Foo", (object,), {})
+
+
+class Bar(Foo):
+    pass
+
+
+obj = Bar()
+print(111)
